f1737b1589664daffb6f9c090bdf3c47c0fcd688
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2024-12-08 23:19:49 +0000

    Fun Facts SPA deployment with Back4App

---
+++ src/api/api.js
@@ -0,0 +1,56 @@
+import { clearUserData, getUserData } from "../views/utils.js";
+
+const host = "https://parseapi.back4app.com";
+const appId = "5IgptbDbqGJyL9jrN0E22H5aNErlrsLT95wsmYj5";
+const apiKey = "zmEGoj97JNp8xgy993p8yEuj5wC4tehdpf6Zqh7H";
+
+async function request(method, url, body) {
+  const options = {
+    method,
+    headers: {
+      "X-Parse-Application-Id": appId,
+      "X-Parse-REST-API-Key": apiKey,
+      "X-Parse-Revocable-Session": 1,
+    },
+  };
+
+  if (body != undefined) {
+    options.headers["Content-Type"] = "application/json";
+    options.body = JSON.stringify(body);
+  }
+
+  const userData = getUserData();
+  if (userData) {
+    const token = userData.sessionToken;
+    options.headers["X-Parse-Session-Token"] = token;
+  }
+
+  try {
+    const res = await fetch(`${host}${url}`, options);
+    let data;
+    if (res.status !== 204) {
+      data = await res.json();
+    }
+
+    if (res.ok == false) {
+      if (data.code === 209) {
+        clearUserData();
+      }
+      const error = data;
+      throw {
+        message: error.error,
+        handled: false,
+      };
+    }
+
+    return data;
+  } catch (error) {
+    alert(error.message);
+    throw error;
+  }
+}
+
+export const get = request.bind(null, "GET");
+export const post = request.bind(null, "POST");
+export const put = request.bind(null, "PUT");
+export const del = request.bind(null, "DELETE");
---
+++ src/api/auth.js
@@ -0,0 +1,55 @@
+import { clearUserData, getUserData, setUserData } from "../views/utils.js";
+import { get, post, del } from "./api.js";
+
+const endpoints = {
+  register: "/users",
+  login: "/login",
+  logout: "/logout",
+  sessionByToken: (token) => `/sessions?${filter("sessionToken", token)}`,
+  sessionById: (id) => `/sessions/${id}`,
+};
+
+export async function registerUser(username, password) {
+  const { objectId, sessionToken } = await post(endpoints.register, {
+    username,
+    password,
+  });
+
+  setUserData({
+    username,
+    objectId,
+    sessionToken,
+  });
+}
+export async function loginUser(username, password) {
+  const { objectId, sessionToken } = await post(endpoints.login, {
+    username,
+    password,
+  });
+  setUserData({
+    username,
+    objectId,
+    sessionToken,
+  });
+}
+
+export async function logoutUser() {
+  const user = getUserData();
+  const sessionToken = user.sessionToken;
+
+  try {
+    const sessions = await get(endpoints.sessionByToken(sessionToken));
+    const [currentSession] = sessions.results;
+    await del(endpoints.sessionById(currentSession.objectId));
+  } catch (error) {
+    alert(error.message);
+  } finally {
+    clearUserData();
+  }
+}
+
+function filter(fieldName, value) {
+  const query = JSON.stringify({ [fieldName]: value });
+
+  return `where=${encodeURIComponent(query)}`;
+}
