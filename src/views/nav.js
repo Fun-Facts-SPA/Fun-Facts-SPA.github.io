350e88970a98d86975d43d452f3259d3d50da938
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2024-12-08 23:29:09 +0000

    corrected the paths to html and page libraries

--- src/views/nav.js
+++ src/views/nav.js
@@ -1,28 +1,26 @@
-import { html } from '../../node_modules/lit-html/lit-html.js'
-import { logoutUser } from '../api/auth.js'
-import page from '../../node_modules/page/page.mjs'
+import { html } from "../../lit-html/lit-html.js";
+import { logoutUser } from "../api/auth.js";
+import page from "../../page/page.mjs";
 
 export const navTemplate = (isUser) => html`
-<div>
-  <a href="/dashboard">Fun Facts</a>
-</div>
-${isUser ? userNav : guestNav}
-`
+  <div>
+    <a href="/dashboard">Fun Facts</a>
+  </div>
+  ${isUser ? userNav : guestNav}
+`;
 
-const userNav = html`
-<div class="user">
+const userNav = html` <div class="user">
   <a href="/create">Add Fact</a>
   <a @click=${logoutSubmit} href="javascript:void(0)">Logout</a>
-</div>`
+</div>`;
 
-const guestNav = html`
-<div class="guest">
+const guestNav = html` <div class="guest">
   <a href="/login">Login</a>
   <a href="/register">Register</a>
-</div>`
+</div>`;
 
 function logoutSubmit(e) {
   e.preventDefault();
   logoutUser();
-  page.redirect('/')
+  page.redirect("/");
 }
